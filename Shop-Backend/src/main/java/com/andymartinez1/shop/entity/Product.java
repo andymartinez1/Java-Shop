64ac196352142c41a55c8e6f58006ffba19471b0
Andrew Martinez <andy72391@gmail.com>
2024-08-28 23:21:13 -0400

    Added product and category entities to the project and created a basic_shop database in MySQL

---
+++ Shop-Backend/src/main/java/com/andymartinez1/shop/entity/Product.java
@@ -0,0 +1,26 @@
+package com.andymartinez1.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+    private int inventory;
+    private String description;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
